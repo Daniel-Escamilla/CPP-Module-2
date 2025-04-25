2fbab4bc6b087122f23b74a649bbf942f0e7b53f
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2025-04-25 12:43:32 +0200

    Add main.cpp file to implement the main function for the Fixed class

---
+++ ex00/src/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daniel-escamilla <daniel-escamilla@stud    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/25 11:04:28 by daniel-esca       #+#    #+#             */
+/*   Updated: 2025/04/25 12:17:04 by daniel-esca      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/fixed.hpp"
+
+int main( void )
+{
+    Fixed a;
+    Fixed b( a );
+    Fixed c;
+    c = b;
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+    return 0;
+}
