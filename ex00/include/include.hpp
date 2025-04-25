2a658a054717f455bd4ef381d1ff214f805d31da
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2025-04-25 12:43:23 +0200

    Add include.hpp header file for project includes

---
+++ ex00/include/include.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   include.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daniel-escamilla <daniel-escamilla@stud    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/25 11:04:32 by daniel-esca       #+#    #+#             */
+/*   Updated: 2025/04/25 11:04:33 by daniel-esca      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INCLUDE_HPP
+# define INCLUDE_HPP
+
+# include <iostream>
+# include <string>
+# include "fixed.hpp"
+
+#endif
