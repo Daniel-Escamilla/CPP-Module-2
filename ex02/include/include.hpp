a8e96fbd39432f42a83f30d9e33e69b63aad8ef5
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-04-26 20:25:52 +0200

    Add include header file for project dependencies

---
+++ ex02/include/include.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   include.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/26 18:25:29 by descamil          #+#    #+#             */
+/*   Updated: 2025/04/26 18:39:59 by descamil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INCLUDE_HPP
+# define INCLUDE_HPP
+
+# include <iostream>
+# include <string>
+# include <cmath>
+# include "Fixed.hpp"
+
+#endif
