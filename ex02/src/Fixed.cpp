23c51abb48548d9b82cdf04b9e839932e8d71bf4
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-04-26 20:25:55 +0200

    Add Fixed class implementation with constructors, destructors, and arithmetic operators

---
+++ ex02/src/Fixed.cpp
@@ -0,0 +1,172 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/26 18:25:34 by descamil          #+#    #+#             */
+/*   Updated: 2025/04/26 19:41:13 by descamil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/Fixed.hpp"
+
+Fixed::Fixed()
+{
+	_fixedPointValue = 0;
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float &number)
+{
+	_fixedPointValue = roundf(number * (1 << _fractional));
+	std::cout << "Float constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int &number)
+{
+	_fixedPointValue = number << _fractional;
+	std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &copy)
+{
+	_fixedPointValue = copy._fixedPointValue;
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+float Fixed::toFloat( void ) const
+{
+	return ((float)_fixedPointValue / (1 << _fractional));
+}
+
+int Fixed::toInt( void ) const
+{
+	return (_fixedPointValue >> _fractional);
+}
+
+Fixed &Fixed::operator=(const Fixed &copy)
+{
+	if (this != &copy)
+	{
+		std::cout << "Copy assignment operator called" << std::endl;
+		_fixedPointValue = copy._fixedPointValue;
+	}
+	return (*this);
+}
+
+bool Fixed::operator>(const Fixed &other)
+{
+	return (_fixedPointValue > other._fixedPointValue);
+}
+
+bool Fixed::operator<(const Fixed &other)
+{
+	return (_fixedPointValue < other._fixedPointValue);
+}
+
+bool Fixed::operator>=(const Fixed &other)
+{
+	return (_fixedPointValue >= other._fixedPointValue);
+}
+
+bool Fixed::operator<=(const Fixed &other)
+{
+	return (_fixedPointValue <= other._fixedPointValue);
+}
+
+bool Fixed::operator==(const Fixed &other)
+{
+	return (_fixedPointValue == other._fixedPointValue);
+}
+
+bool Fixed::operator!=(const Fixed &other)
+{
+	return (_fixedPointValue != other._fixedPointValue);
+}
+Fixed Fixed::operator+(const Fixed &other) const
+{
+    return Fixed(this->toFloat() + other.toFloat());
+}
+
+Fixed Fixed::operator-(const Fixed &other) const
+{
+    return Fixed(this->toFloat() - other.toFloat());
+}
+
+Fixed Fixed::operator*(const Fixed &other) const
+{
+    return Fixed(this->toFloat() * other.toFloat());
+}
+
+Fixed Fixed::operator/(const Fixed &other) const
+{
+    return Fixed(this->toFloat() / other.toFloat());
+}
+
+Fixed& Fixed::operator++()
+{
+	this->_fixedPointValue++;
+	return *this;
+}
+
+Fixed Fixed::operator++(int)
+{
+	Fixed temp(*this);
+	this->_fixedPointValue++;
+	return temp;
+}
+
+
+Fixed& Fixed::operator--()
+{
+	this->_fixedPointValue--;
+	return *this;
+}
+
+Fixed Fixed::operator--(int)
+{
+	Fixed temp(*this);
+	this->_fixedPointValue--;
+	return temp;
+}
+
+Fixed &Fixed::min(Fixed &a, Fixed &b)
+{
+	if (a < b)
+		return (a);
+	return (b);
+}
+
+const Fixed &Fixed::min(const Fixed &a, const Fixed &b)
+{
+	if (&a < &b)
+		return (a);
+	return (b);
+}
+
+Fixed &Fixed::max(Fixed &a, Fixed &b)
+{
+	if (a < b)
+		return (b);
+	return (a);
+}
+
+const Fixed &Fixed::max(const Fixed &a, const Fixed &b)
+{
+	if (&a < &b)
+		return (b);
+	return (a);
+}
+
+std::ostream& operator<<(std::ostream &stream, const Fixed &fixed)
+{
+	stream << fixed.toFloat();
+	return (stream);
+}
