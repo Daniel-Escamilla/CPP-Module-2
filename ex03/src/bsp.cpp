ca2c9561be77418d8095c4a974acffe916408c72
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-04-30 20:09:31 +0200

    Fix area calculation in bsp function and update precision threshold

--- ex03/src/bsp.cpp
+++ ex03/src/bsp.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   bsp.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daniel-escamilla <daniel-escamilla@stud    +#+  +:+       +#+        */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 11:21:28 by daniel-esca       #+#    #+#             */
-/*   Updated: 2025/04/28 20:47:24 by daniel-esca      ###   ########.fr       */
+/*   Updated: 2025/04/30 20:09:01 by descamil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 float	area(Point p1, Point p2, Point p3)
 {
-	float	part1 = p1.getX() * p2.getX() - p3.getX();
-	float	part2 = p2.getX() * p3.getX() - p1.getX();
-	float	part3 = p3.getX() * p1.getX() - p2.getX();
+	float	part1 = p1.getX() * (p2.getY() - p3.getY());
+	float	part2 = p2.getX() * (p3.getY() - p1.getY());
+	float	part3 = p3.getX() * (p1.getY() - p2.getY());
 	float	total = part1 + part2 + part3;
 	return (fabs(total) / 2.0f);
 }
@@ -28,10 +28,10 @@ bool	bsp( Point const a, Point const b, Point const c, Point const point)
 	float	pointAreaA = area(point, b, c);	
 	float	pointAreaB = area(a, point, c);	
 	float	pointAreaC = area(a, b, point);	
-	double	pointArea = fabs((pointAreaA + pointAreaB + pointAreaC) - originalArea);
+	float	pointArea = fabs((pointAreaA + pointAreaB + pointAreaC) - originalArea);
 
 	std::cout << pointArea << std::endl;
 	
-	return (pointArea < 1);
+	return (pointArea < 0.0001);
 }
 
