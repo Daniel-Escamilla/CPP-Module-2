54671b6747632149d1e630c2b5d67c5f88e8cd85
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2025-04-30 19:26:06 +0200

    Add include.hpp header file with include guards and necessary includes

---
+++ ex03/include/include.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   include.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daniel-escamilla <daniel-escamilla@stud    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/28 11:20:49 by daniel-esca       #+#    #+#             */
+/*   Updated: 2025/04/28 20:18:14 by daniel-esca      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INCLUDE_HPP
+# define INCLUDE_HPP
+
+# include <iostream>
+# include <string>
+# include <cmath>
+
+# include "Fixed.hpp"
+# include "Point.hpp"
+
+#endif
