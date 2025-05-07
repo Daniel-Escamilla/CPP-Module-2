39efb9e08f37469995979a6c34dd227625ff2cc5
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-05-07 15:27:59 +0200

    chore: Refactor Fixed class constructors and assignment operator

--- ex02/include/Fixed.hpp
+++ ex02/include/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 18:26:38 by descamil          #+#    #+#             */
-/*   Updated: 2025/04/26 19:38:29 by descamil         ###   ########.fr       */
+/*   Updated: 2025/05/07 12:49:00 by descamil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,15 @@ class Fixed
 		int					_fixedPointValue;
 	public:
 		Fixed();
+		~Fixed();
+		Fixed(const Fixed &copy);
+		Fixed &operator=(const Fixed &copy);
 		Fixed(const float &number);
 		Fixed(const int &number);
-		Fixed(const Fixed &copy);
-		~Fixed();
 
 		float toFloat( void ) const;
 		int toInt( void ) const;
 
-		Fixed &operator=(const Fixed &copy);
 
 		bool operator>(const Fixed &copy);
 		bool operator<(const Fixed &copy);
